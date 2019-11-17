4e951c1923bd20266aade982c5030ec4120cce7b
Justin Phillips <justinphil02@yahoo.com>
2019-11-17 23:07:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |stuent|
       Student.new(student)
-    end 
+    end
   end
 
   def add_student_attributes(attributes_hash)
