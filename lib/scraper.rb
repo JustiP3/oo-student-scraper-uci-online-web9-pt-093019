c349572f6d465f18c35dbba05213908908167da7
Justin Phillips <justinphil02@yahoo.com>
2019-11-17 22:59:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -41,8 +41,8 @@ class Scraper
     end
     bio = profile_page.css("div.bio-content.content-holder div.description-holder p").text
     quote = profile_page.css("div.vitals-text-container div.profile-quote").text
-    student_hash[:profile_quote] = quote 
-    student_hash[:bio] = bio 
+    student_hash[:profile_quote] = quote
+    student_hash[:bio] = bio
   end
 
 
